767e86ff0f1ee150ec4902a41e628e521bd38c10
Kemily Teixeira <101020416+kmlyteixeira@users.noreply.github.com>
2022-04-25 13:29:34 -0300

    Update and rename exemploSwitchCase.java to ExemploSwitchCase.java

--- ExemploSwitchCase.java
+++ ExemploSwitchCase.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class exemploSwitchCase {
+public class ExemploSwitchCase {
     public static void main(String[] args) {
         //Fazer um programa para ler um valor inteiro de 1 a 7 representando um
         //dia da semana (sendo 1=domingo, 2=segunda, e assim por diante).
