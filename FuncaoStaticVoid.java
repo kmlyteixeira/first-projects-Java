15fff25898ef8bc0bda71e1c3cb31e702d0f2db4
Kemily Teixeira <101020416+kmlyteixeira@users.noreply.github.com>
2022-04-27 13:34:24 -0300

    Add files via upload

---
+++ FuncaoStaticVoid.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+//Exemplo de criação de dois métodos
+// 1º MAIOR VALOR Static - retorna valor a ser utilizado na Main
+// 2º MOSTRA RESULTADO Void - não retorna um valor 
+
+public class FuncaoStaticVoid {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println(" ---- O MAIOR ENTRE TRÊS NUMEROS ---- ");
+        int i, a = 0, b = 0, c = 0;
+        for (i = 1; i <= 3; i++) {
+            System.out.println("Enter the " + i + "º number");
+            if (i == 1) {
+                a = sc.nextInt();
+            } else if (i == 2) {
+                b = sc.nextInt();
+            } else if (i == 3) {
+                c = sc.nextInt();
+            }
+        }
+
+        int higher = max(a, b, c);
+        showResult(higher);
+
+        sc.close();
+    }
+
+    public static int max(int x, int y, int z) {
+        int aux;
+        if (x > y && x > z) {
+            aux = x;
+        } else if (y > z) {
+            aux = y;
+        } else {
+            aux = z;
+        }
+        return aux;
+    }
+
+    public static void showResult(int value) {
+        System.out.println("Higher = " + value);
+    }
+}
