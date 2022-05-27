76e625c7f4a7b765fa38ac61df12c8ac8fdbb1f1
Kemily Teixeira da Rosa <kemily.rosa@orbenk.br>
2022-05-27 13:31:47 -0300

    aula 67 - Orientacao a Objetos

---
+++ EntitieProduct/application/Program.java
@@ -0,0 +1,18 @@
+package application;
+
+import java.util.Locale;
+
+import entities.Product;
+
+public class Program {
+    public static void main(String[] args) {
+        
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(system.in);
+
+        Product product = new Product();
+        
+        /* FINALIZAR PROGRAMA */
+
+    }
+}
